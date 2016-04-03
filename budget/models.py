68747c82df787d040daca37fd8e8540553faa98a
prodge <prodge@prodge.net>
2016-04-03 22:24:51 +0800

    Reordered models

--- budget/models.py
+++ budget/models.py
@@ -20,6 +20,8 @@ class Category(models.Model):
     parent = models.ForeignKey(
         'Category',
         on_delete = models.CASCADE,
+        blank=True,
+        null=True,
     )
 
 
@@ -27,21 +29,10 @@ class Entry(models.Model):
     '''
     A journal entry
     '''
-    # The category of the Entry
-    category = models.ForeignKey(
-        'Category',
-        on_delete = models.CASCADE,
-    )
-
     # A string describing the entry
     label = models.CharField(
         max_length = settings.MAX_NAME_LENGTH,
         blank = False,
-    )
-
-    # A more detailed description of the Entry (not required)
-    description = models.CharField(
-        max_length = settings.MAX_DESCRIPTION_LENGTH,
     )
 
     # The date of the entry
@@ -50,13 +41,29 @@ class Entry(models.Model):
         blank = False,
     )
 
+    # Is the entry an expense or income
+    flow_type = models.CharField(
+        choices = (
+            ('income', 'Income'),
+            ('expense', 'Expense'),
+        ),
+        max_length = 50,
+        default = 'expense',
+    )
+
+    # The category of the Entry (Required. User is encouraged to create an uncategorized category)
+    category = models.ForeignKey(
+        'Category',
+        on_delete = models.CASCADE,
+    )
+
+    # A more detailed description of the Entry (not required)
+    description = models.CharField(
+        max_length = settings.MAX_DESCRIPTION_LENGTH,
+    )
+
     # Is the expense essential (required but not considered when processing income)
     essential = models.BooleanField(
         default = False,
-    )
-
-    # Is the entry an expense or income
-    is_expense = models.BooleanField(
-        default = True,
     )
 
