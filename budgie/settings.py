aa65f7726afb535a39daf9330dbd7fd2307c17b1
prodge <prodge@prodge.net>
2016-04-03 21:58:26 +0800

    Added new template directory

--- budgie/settings.py
+++ budgie/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'budgie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'budgie/templates/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
