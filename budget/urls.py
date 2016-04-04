8d76b93957eeed46838b488fc9c810e55378512f
prodge <prodge@prodge.net>
2016-04-04 22:02:25 +0800

    Added urls and views for editing and creating entries

--- budget/urls.py
+++ budget/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
         views.entry_list,
         name = 'entry_list'
     ),
+    url(
+        r'^entry/(?P<pk>\d+)/',
+        views.entry_edit,
+        name = 'entry_edit'
+    ),
+    url(
+        r'^entry-create/',
+        views.entry_create,
+        name = 'entry_create'
+    ),
 ]
--- budget/views.py
+++ budget/views.py
@@ -2,15 +2,41 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from budget.models import Entry
+from budget.forms import EntryForm
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = 'index.html'
+
+    return render(request, template, context)
 
 def entry_list(request):
     template = 'entry_list.html'
 
-    context={
-        'entries': Entry.objects.all()
+    context = {
+        'entries': Entry.objects.all(),
     }
+
+    return render(request, template, context)
+
+def entry_edit(request):
+    template = 'entry_edit.html'
+
+
+    return render(request, template, context)
+
+def entry_create(request):
+    template = 'entry_create.html'
+
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+
+        form = EntryForm()
+
+        context = {
+            'form': form,
+        }
 
     return render(request, template, context)
