f1b8aca483c7f29bbbd2994063d9a5632e26e30a
Prodge <prodge@prodge.net>
2016-04-24 19:53:52 +0800

    Added jquery datepicker widget

--- budget/forms.py
+++ budget/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from budget.models import Entry
+from functools import partial
+
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
         fields = '__all__'
+        widgets = {'date': DateInput(),}
+
