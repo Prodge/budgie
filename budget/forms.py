b6d257df4011263736103022c890de5f8df28304
Prodge <prodge@prodge.net>
2016-06-07 02:21:15 +0800

    Added category form

--- budget/forms.py
+++ budget/forms.py
@@ -16,3 +16,9 @@ class EntryForm(forms.ModelForm):
         exclude = ('user', )
         widgets = {'date': DateInput(),}
 
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        exclude = ('user', )
+
