386b0d62716e59ebcad056a25d91c42103d102bd
Prodge <prodge@prodge.net>
2016-06-07 01:45:09 +0800

    Fixed url pattern matching

--- budget/urls.py
+++ budget/urls.py
@@ -9,22 +9,22 @@ urlpatterns = [
         name = 'home'
     ),
     url(
-        r'^entry-list/',
+        r'^entry-list/$',
         views.entry_list,
         name = 'entry_list'
     ),
     url(
-        r'^entry/(?P<entry_id>\d+)/',
+        r'^entry/(?P<entry_id>\d+)/$',
         views.entry_detail,
         name = 'entry_detail'
     ),
     url(
-        r'^entry/(?P<entry_id>\d+)/edit/',
+        r'^entry/(?P<entry_id>\d+)/edit/$',
         views.entry_edit,
         name = 'entry_edit'
     ),
     url(
-        r'^entry-create/',
+        r'^entry/create/$',
         views.entry_create,
         name = 'entry_create'
     ),
