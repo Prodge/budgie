bd4224d3d2e19ea264487644ed439d33e2bee25d
Prodge <prodge@prodge.net>
2018-01-01 23:06:12 +0800

    Don't require csrf for api

--- assistant/api.py
+++ assistant/api.py
@@ -3,10 +3,12 @@ import json
 
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from budget.models import Entry
 
 # @login_required
+@csrf_exempt
 def new_entry(request):
     assert request.method == 'POST', 'Request must be a POST'
 
