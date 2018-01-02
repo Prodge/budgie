a371387601bafb72fbe4db27f180d1cb02432d25
Prodge <prodge@prodge.net>
2018-01-02 23:21:55 +0800

    Initial working assistant api

--- assistant/api.py
+++ assistant/api.py
@@ -1,21 +1,38 @@
-import datetime
 import json
+
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 
-from budget.models import Entry
+from budget.models import Entry, Category
 
 # @login_required
 @csrf_exempt
 def new_entry(request):
     assert request.method == 'POST', 'Request must be a POST'
 
+    body = json.loads(request.body.decode('utf-8'))
+
+    raw_query = body['originalRequest']['data']['inputs'][0]['arguments'][0]['rawText']
+    parameters = body['result']['parameters']
+
+    entry = Entry(
+        label = paramaters['category'],
+        value = parameters['unit-currency']['amount'],
+        date = datetime.strptime(body['result']['parameters']['date'], '%Y-%m-%d'),
+        flow_type = parameters['flow_type'],
+        category = Category.objects.get(name__iexact=paramaters['category']),
+        User = User.objects.get(username='tim'),
+    )
+    entry.save()
+
     return JsonResponse({
-        'speech': 'Entry has been added',
-        'displayText': 'Entry has been added',
-        'data': {},
+        'speech': 'Entry for {} of {} has been added'.format(entry.value),
+        'displayText': 'Entry for {} of {} has been added'.format(entry.label),
+        'data': entry.__dict__,
         'contextOut': [],
         'source': "Budgie Money",
     })
