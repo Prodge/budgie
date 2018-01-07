eeec3aa4559eba537d656eda6d82eeb4cf23d43a
Prodge <prodge@prodge.net>
2018-01-07 12:26:01 +0800

    Implement expense query assistant api

--- assistant/api.py
+++ assistant/api.py
@@ -7,6 +7,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from budget.models import Entry, Category
+from budget.views import get_total_value
 from assistant.decorators import dialogflow_auth_required
 
 
@@ -35,7 +36,7 @@ def new_entry(request, parameters=[], **kwargs):
     entry = Entry(
         label = parameters['label'],
         value = parameters['unit-currency']['amount'],
-        date = datetime.strptime(parameters['date'], '%Y-%m-%d'),
+        date = cast_dialogflow_date(parameters['date']),
         flow_type = parameters['flow_type'],
         category = Category.objects.get(name__iexact=parameters['category']),
         user = request.user,
@@ -52,5 +53,53 @@ def new_entry(request, parameters=[], **kwargs):
     })
 
 
-def expense_query(request, parameters=[]):
-    pass
+def get_total_expense_over_range(user, start_date, end_date):
+    return get_total_value(
+        Entry.objects.get(
+            user = user,
+            date__gte = start_date,
+            date__lte = end_date,
+        )
+    )
+
+
+def cast_dialogflow_date(date_string):
+    return datetime.strptime(date_string, '%Y-%m-%d')
+
+
+def expense_query(request, parameters=[], **kwargs):
+    try:
+        category = Category.objects.get(name__iexact=parameters['category'])
+    except Category.DoesNotExist:
+        category = None
+
+    if parameters.get('date'):
+        start_date = end_date = cast_dialogflow_date(parameters['date'])
+
+    if parameters.get('date-period'):
+        start_date, end_date = map(cast_dialogflow_date, parameters.get('date-period').split('/'))
+
+    total_spent = get_total_expenses_over_range(
+        request.user,
+        start_date,
+        end_date
+    )
+
+    date_period_user_string = body['result']['contexts'][0]['date-period.original']
+    date_user_string = body['result']['contexts'][0]['date.original']
+
+    response_text = 'You spent {} {} {} {}'.format(
+        total_spent,
+        'on {}'.format(caterogy.name) if category else '',
+        'over {}'.format(date_period_user_string),
+        date_user_string
+    )
+
+    return JsonResponse({
+        'speech': response_text,
+        'displayText': response_text,
+        'data': {},
+        'contextOut': [],
+        'source': "Budgie Money",
+    })
+
